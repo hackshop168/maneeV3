e44b994a4f5e86b004b9debecb73e7bf79a165c1
hackshop168 <hackshop1688@gmail.com>
2026-08-25 05:21:08 +0700

    Update vite.config.js

--- public/vite.config.js
+++ public/vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000
-  }
 })
